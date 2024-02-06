6c0946cfb0bd350cf06906fd1a7e47f04cf8f6ff
Austin Reilly <areilly@bitwiseindustries.com>
2024-02-05 17:56:11 -0800

    fix(remote-time): update time accurately remotely

--- javascript.js
+++ javascript.js
@@ -1,15 +1,16 @@
-const countDownDate = new Date('2024-03-01T18:00:00')
-const x = setInterval(()=>{
-	const now 			= new Date().getTime()
-	const distance	= countDownDate - now
-	const days 			= Math.floor(distance / (1000 * 60 * 60 * 24))
-	const hours 		= Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-	const minutes 	= Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-	const seconds 	= Math.floor((distance % (1000 * 60)) / 1000)
-	document.getElementById('countdown').innerHTML = days + 'd ' + hours + 'h '
-	+ minutes + 'm ' + seconds + 's '
-	if (distance < 0) {
-		clearInterval(x)
-		document.getElementById('countdown').innerHTML = 'YELL'
+const targetDate = new Date("2024-03-01T18:00:00-08:00")
+const countdown = setInterval(()=>{
+	const now = new Date()
+	const timeDiff = targetDate - now
+	if(timeDiff <= 0){
+		clearInterval(countdown)
+		document.getElementById("countdown").innerHTML = "Countdown Finished!"
+		return
 	}
-}, 1000);
+	const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
+	const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+	const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60))
+	const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000)
+	const countdownDisplay = days + "d " + hours + "h " + minutes + "m " + seconds + "s"
+document.getElementById("countdown").innerHTML = countdownDisplay
+}, 1000)
